f3878162e988930b2c6a1a2e10e9d9c21f81989f
satyasheel gatuam <satyasheelgautam87@gmial.com>
2026-04-16 22:19:56 +0530

    api added

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
+const API = axios.create({ baseURL: 'https://shopeasy-api.onrender.com/api' });
 
 // Attach JWT token to every request automatically
 API.interceptors.request.use((config) => {
